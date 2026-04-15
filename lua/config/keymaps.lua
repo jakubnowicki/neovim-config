9f0a2f2576ec18ffbecc66454e4a39d879707c2b
Jakub Nowicki <kuba@appsilon.com>
2026-04-15 14:32:06 +0200

    add terminal keymaps and window navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,24 @@ map("n", "<leader>w", "<cmd>w<cr>")
 
 -- exit terminal mode
 map("t", "<Esc>", "<C-\\><C-n>")
+
+-- open terminal (horizontal split)
+map("n", "<leader>th", function()
+  vim.cmd("split")
+  vim.cmd("terminal")
+end)
+
+-- open terminal (vertical split)
+map("n", "<leader>tv", function()
+  vim.cmd("vsplit")
+  vim.cmd("terminal")
+end)
+
+-- window navigation
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-l>", "<C-w>l")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+
+-- close current window
+map("n", "<leader>q", "<cmd>close<cr>")
