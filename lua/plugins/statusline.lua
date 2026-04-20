bcc5f7cb5de3d8a22dcc5fc760187379446cdcac
Jakub Nowicki <kuba@appsilon.com>
2026-04-20 13:11:51 +0200

    Improve git in statusline.

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -2,6 +2,33 @@ return {
   "echasnovski/mini.statusline",
   version = false,
   config = function()
-    require("mini.statusline").setup()
+    local statusline = require("mini.statusline")
+
+    statusline.setup({
+      use_icons = false,
+      content = {
+        active = function()
+          local mode = statusline.section_mode({ trunc_width = 120 })
+          local git = statusline.section_git({ trunc_width = 40 })
+          local diff = statusline.section_diff({ trunc_width = 75 })
+          local diagnostics = statusline.section_diagnostics({ trunc_width = 75 })
+          local lsp = statusline.section_lsp({ trunc_width = 75 })
+          local filename = statusline.section_filename({ trunc_width = 140 })
+          local fileinfo = statusline.section_fileinfo({ trunc_width = 120 })
+          local location = statusline.section_location({ trunc_width = 75 })
+          local search = statusline.section_searchcount({ trunc_width = 75 })
+
+          return statusline.combine_groups({
+            { hl = "MiniStatuslineModeNormal", strings = { mode } },
+            { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
+            "%<",
+            { hl = "MiniStatuslineFilename", strings = { filename } },
+            "%=",
+            { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+            { hl = "MiniStatuslineFilename", strings = { search, location } },
+          })
+        end,
+      },
+    })
   end,
 }
